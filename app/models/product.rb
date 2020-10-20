78b13ba3d3e129726fd4806ad4e2155cc02ad34e
Daniel <danieldelnaaij@gmail.com>
2020-10-20 17:47:05 +0200

    Add product field validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+    validates :reference, uniqueness: true
+    validates :name, presence: true, uniqueness: {scope: :category}
+    validates :category, presence: true
+    validates :price, numericality: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_uniqueness_of(:reference)}
+  it { is_expected.to validate_uniqueness_of(:name).scoped_to(:category)}
+  it { is_expected.to validate_presence_of(:name)}
+  it { is_expected.to validate_presence_of(:category)}
+  it { is_expected.to validate_numericality_of(:price)}
 end
