25f36ba723b397ab8fb24c27d3951b550837d038
Daniel <danieldelnaaij@gmail.com>
2020-10-21 11:37:15 +0200

    Criteria definition output formatting

--- app/models/criteria_definition.rb
+++ app/models/criteria_definition.rb
@@ -1,4 +1,18 @@
 class CriteriaDefinition < ApplicationRecord
   validates :destination, presence: true
   validates :max_product_price, numericality: true, allow_nil: true
+
+  def to_s
+    references = output_array_string(product_references)
+    categories = output_array_string(product_categories)
+    price = max_product_price&.round(2) || "_"
+
+    "[ %s, %s, %s ] -> %s" % [references, categories, price, destination]
+  end
+
+  private
+
+  def output_array_string(attribute)
+    attribute.present? ? "[#{attribute.join(", ")}]" : "_"
+  end
 end
--- spec/models/criteria_definition_spec.rb
+++ spec/models/criteria_definition_spec.rb
@@ -17,4 +17,36 @@ RSpec.describe CriteriaDefinition, type: :model do
   describe "#product_categories" do
     it_behaves_like "an array", :product_categories
   end
+
+  describe "#to_s" do
+    it "outputs the criteria definition in a predefined format" do
+      definition =
+        described_class.new(
+          product_references: %W[REF_1 REF_2],
+          product_categories: %W[CAT_1 CAT_2],
+          max_product_price: 100.00,
+          destination: "DEST_1"
+        )
+
+      expected = "[ [REF_1, REF_2], [CAT_1, CAT_2], 100.0 ] -> DEST_1"
+
+      expect(definition.to_s).to eql expected
+    end
+
+    context "when attributes are empty" do
+      it "outputs the criteria definition in a predefined format" do
+        definition =
+          described_class.new(
+            product_references: [],
+            product_categories: [],
+            max_product_price: nil,
+            destination: "DEST_1"
+          )
+
+        expected = "[ _, _, _ ] -> DEST_1"
+
+        expect(definition.to_s).to eql expected
+      end
+    end
+  end
 end
